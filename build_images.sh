47b881eee7ef2456946ecf73dee7fd3e47087d20
vliedel <bart@dobots.nl>
2013-12-10 15:12:02 +0100

    decreased image size

--- build_images.sh
+++ build_images.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-docker build -t dobots/aim_base - < aim-base-img.txt
-docker build -t dobots/aim_zmq - < aim-zmq-img.txt
-docker build -t dobots/aim_dodedodo - < aim-dodedodo-img.txt
+docker build -t dobots/aim_base - < aim-base-img.txt && docker build -t dobots/aim_zmq - < aim-zmq-img.txt && docker build -t dobots/aim_dodedodo - < aim-dodedodo-img.txt
