00be7e092b2c6bdbfff24c76832f03e13248a7de
Vliedel <bart@dobots.nl>
2014-11-05 11:46:28 +0100

    targets for build and run of images

--- build_images.sh
+++ build_images.sh
@@ -1,3 +1,50 @@
 #!/bin/bash
 
-docker build -t dobots/aim_base - < aim-base-img.txt && docker build -t dobots/aim_zmq - < aim-zmq-img.txt && docker build -t dobots/aim_dodedodo - < aim-dodedodo-img.txt
+# TODO: use a makefile instead?
+
+declare -a targets=("aim-base" "aim-zmq" "aim-nodejs" "aim-yarp" "aim-ros" "aim-dodedodo" "inertia")
+
+if [ $# -lt 1 ] ; then
+	echo "Usage: $0 target..."
+	echo "targets: all ${targets[@]}"
+	exit
+fi
+
+if [ "$1" == "all" ] ; then
+	build=(${targets[@]})
+else
+	build=($@)
+fi
+
+
+for i in "${build[@]}"
+do
+	if [ "$i" == "aim-base" ] ; then
+		echo "build $i"
+		sudo docker build -t dobots/aim_base - < aim-base-img.txt
+	fi
+
+	if [ "$i" == "aim-nodejs" ] ; then
+		echo "build $i (requires: aim-base)"
+		sudo docker build -t dobots/aim_nodejs - < aim-nodejs-img.txt
+	fi
+
+	if [ "$i" == "aim-zmq" ] ; then
+		echo "build $i (requires: aim-base)"
+		sudo docker build -t dobots/aim_zmq - < aim-zmq-img.txt
+	fi
+
+	if [ "$i" == "aim-dodedodo" ] ; then
+		echo "build $i (requires: aim-zmq)"
+		sudo docker build -t dobots/aim_dodedodo - < aim-dodedodo-img.txt
+	fi
+
+
+	if [ "$i" == "inertia" ] ; then
+		echo "build inertia (requires: aim-nodejs)"
+		sudo docker build -t dobots/inertia - < inertia-img.txt
+		sudo docker build -t dobots/inertia_java - < inertia-java-img.txt
+	fi
+done
+
+
--- run.sh
+++ run.sh
@@ -1,1 +1,37 @@
-sudo docker run -e XMPP_NAMESERVER_USERNAME=$DODEDODO_USERNAME -e XMPP_NAMESERVER_PASSWORD=$DODEDODO_PASSWORD -e XMPP_NAMESERVER_RESOURCE=$DODEDODO_RESOURCE -e AIM_WORKSPACE=/aim_workspace  -i --privileged -v /dev/rfxcom:/dev/rfxcom -t dobots/aim_dodedodo bash
+#!/bin/bash
+
+declare -a targets=("aim-base" "aim-zmq" "aim-nodejs" "aim-yarp" "aim-ros" "aim-dodedodo" "inertia")
+
+if [ $# -lt 1 ] ; then
+        echo "Usage: $0 target"
+        echo "targets: ${targets[@]}"
+        exit
+fi
+
+
+
+if [ "$i" == "aim-base" ] ; then
+	echo "run $i"
+	sudo docker run -e AIM_WORKSPACE=/aim_workspace -i -t dobots/aim_base bash
+fi
+
+if [ "$i" == "aim-nodejs" ] ; then
+	echo "run $i"
+	sudo docker run -e AIM_WORKSPACE=/aim_workspace -i -t dobots/aim_nodejs bash
+fi
+
+if [ "$i" == "aim-zmq" ] ; then
+	echo "run $i"
+	sudo docker run -e AIM_WORKSPACE=/aim_workspace -i -t dobots/aim_zmq bash
+fi
+
+if [ "$i" == "aim-dodedodo" ] ; then
+	echo "run $i"
+	sudo docker run -e XMPP_NAMESERVER_USERNAME=$DODEDODO_USERNAME -e XMPP_NAMESERVER_PASSWORD=$DODEDODO_PASSWORD -e XMPP_NAMESERVER_RESOURCE=$DODEDODO_RESOURCE -e AIM_WORKSPACE=/aim_workspace -i --privileged -v /dev/rfxcom:/dev/rfxcom -t dobots/aim_dodedodo bash
+fi
+
+
+if [ "$i" == "inertia" ] ; then
+	echo "run $i"
+	sudo docker run -p 49160:8090 -d dobots/inertia_java
+fi
